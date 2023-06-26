9a15119dee34b65c1fa653150debcf767af4acf3
Gabrielle Schryver <gabbyschryver@gmail.com>
2023-06-26 15:00:18 -0400

    chapter 7 complete

--- Models/Dog.cs
+++ Models/Dog.cs
@@ -1,12 +1,24 @@
-﻿namespace DogGo.Models
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace DogGo.Models
 {
     public class Dog
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Please enter a name for the dog.")]
+        [MaxLength(35, ErrorMessage = "The name cannot exceed 35 characters.")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "Please select an owner.")]
+        [DisplayName("Owner")]
         public int OwnerId { get; set; }
+        [Required]
         public string Breed { get; set; }
+        [Required]
         public string Notes { get; set; }
+        [Required]
+        [Display(Name="Dog's Image URL")]
+        [DataType(DataType.ImageUrl, ErrorMessage = "Please enter a valid Image URL.")]
         public string ImageUrl { get; set; }
         public Owner Owner { get; set; }
     }
--- Models/Walker.cs
+++ Models/Walker.cs
@@ -1,11 +1,24 @@
-﻿namespace DogGo.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DogGo.Models
 {
     public class Walker
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please enter the walker's name.")]
+        [MaxLength(50, ErrorMessage = "The name cannot exceed 50 characters.")]
+        [Display(Name = "Name")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please select a neighborhood.")]
+        [Display(Name = "Neighborhood")]
         public int NeighborhoodId { get; set; }
+
+        [Display(Name = "Image URL")]
+        [DataType(DataType.ImageUrl, ErrorMessage = "Please enter a valid image URL.")]
         public string ImageUrl { get; set; }
+
         public Neighborhood Neighborhood { get; set; }
     }
 }
